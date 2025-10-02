d8aa013e7ba7f8f0c0b7ed40a703a1e2642ab31d
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2025-10-02 17:21:34 -0400

    chore: update lint-staged configuration to use parallel execution for pint

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 const config = {
     "**/*.php": [
         "php ./vendor/bin/rector --dry-run --ansi",
-        "php ./vendor/bin/pint --test --ansi",
+        "php ./vendor/bin/pint --parallel --test --ansi",
         "php ./vendor/bin/phpstan analyse --memory-limit=2G --ansi",
         "php ./vendor/bin/pest --colors=always --coverage --parallel"
     ],
